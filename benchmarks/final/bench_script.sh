dcc5e0defec76ff21671c25e77edae6f956bb0a7
stlenyk <stlenyk@gmail.com>
2024-06-18 02:45:24 +0200

    Fix benchmark script

--- benchmarks/final/bench_script.sh
+++ benchmarks/final/bench_script.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# N_SMALL="25,50,100,200,400,800,1600,3200,6400"
-# N_LARGE="25,50,100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800"
-
-# make N_SMALL and N_LARGE smallery by a factor of 10
-N_SMALL="2,5,10,20,40,80,160,320,640"
-N_LARGE="2,5,10,20,40,80,160,320,640,1280,2560,5120,10240,20480"
+N_SMALL="10,20,50,100,200,400,800,1600,3200,6400"
+N_LARGE="10,20,50,100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800"
 
 HYP_N_WARMUP=3
 HYP_N_RUNS=10
@@ -45,13 +41,13 @@ hyperfine "python $ALG_DIR/my_sa.py cuda {n_dim}" \
     --warmup $HYP_N_WARMUP \
     --runs $HYP_N_RUNS \
     --export-csv $RES_DIR/"my_sa_cuda.csv" \
-    --parameter-list n_population $N_LARGE
+    --parameter-list n_dim "10,20,50,100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800,51200,102400,204800,409600,819200,1638400,3276800,6553600,13107200,26214400,52428800,104857600"
 
 hyperfine "python $ALG_DIR/my_sa.py cpu {n_dim}" \
     --warmup $HYP_N_WARMUP \
     --runs $HYP_N_RUNS \
     --export-csv $RES_DIR/"my_sa_cpu.csv" \
-    --parameter-list n_dim $N_SMALL
+    --parameter-list n_dim "10,20,50,100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800,102400,204800,409600,819200,1638400,3276800,6553600"
 
 ###############################################################################
 # theirs                                                                      #
@@ -67,10 +63,10 @@ hyperfine "python $ALG_DIR/their_abc.py 500 {n_population}" \
     --warmup $HYP_N_WARMUP \
     --runs $HYP_N_RUNS \
     --export-csv $RES_DIR/"their_abc.csv" \
-    --parameter-list n_population $N_SMALL
+    --parameter-list n_population "10,20,50,100,200,400"
 
 hyperfine "python $ALG_DIR/their_sa.py {n_dim}" \
     --warmup $HYP_N_WARMUP \
     --runs $HYP_N_RUNS \
     --export-csv $RES_DIR/"their_sa.csv" \
-    --parameter-list n_dim $N_SMALL
+    --parameter-list n_dim "10,20,50,100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800"

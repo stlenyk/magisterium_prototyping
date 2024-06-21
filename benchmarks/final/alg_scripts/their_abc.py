ce5c2a6037d819383e0b2ad9dcf1e78176d470c9
stlenyk <stlenyk@gmail.com>
2024-06-21 04:37:27 +0200

    Unify their_abc settings with other bench scripts

--- benchmarks/final/alg_scripts/their_abc.py
+++ benchmarks/final/alg_scripts/their_abc.py
@@ -1,40 +1,32 @@
 # github page: https://abcolony.github.io/
 # github repository: https://github.com/abcolony/ABCPython
 
-import datetime
+
 import sys
-import time
-from ABCPython import ABC
-from ABCPython import Config
+from ABCPython import ABC, Config
 import numpy as np
+
+
+def one_max(x):
+    return (-np.sum(x) / len(x) * 100,)
 
 
 abc_conf = Config.Config([])  # empy argv
 
 abc_conf.DIMENSION = int(sys.argv[1])
 abc_conf.FOOD_NUMBER = int(sys.argv[2])
-
-n_steps = 1000
+abc_conf.LOWER_BOUND = 0
+abc_conf.UPPER_BOUND = 1
+abc_conf.OBJECTIVE_FUNCTION = one_max
 
 # tweak, so that it doesnt't stop before n_steps
 abc_conf.MAXIMUM_EVALUATION = 1_000_000_000_000
 abc_conf.RUN_TIME = 1
 abc_conf.SHOW_PROGRESS = False
 
+n_steps = 1000
 
-def one_max(x):
-    return -np.sum(x) / len(x) * 100
-
-
-experiment_name = (
-    datetime.datetime.now()
-    .strftime("%Y-%m-%d %H:%M:%S")
-    .replace(" ", "")
-    .replace(":", "")
-)
 abc = ABC.ABC(abc_conf)
-abc.set_experiment_id(1, experiment_name)
-start_time = time.time() * 1000
 abc.initial()
 abc.memorize_best_source()
 for _ in range(n_steps):
@@ -44,3 +36,5 @@ for _ in range(n_steps):
     abc.memorize_best_source()
     abc.send_scout_bees()
     abc.increase_cycle()
+
+print(-abc.globalOpt)

a46ac8bde05c10b9f4a11fbfdd35a0123222c559
stlenyk <stlenyk@gmail.com>
2024-09-18 20:46:45 +0200

    More results data

--- benchmarks/final/bench_script.sh
+++ benchmarks/final/bench_script.sh
@@ -23,7 +23,7 @@ hyperfine "python $ALG_DIR/my_cro.py cpu 500 {n_population}" \
     --warmup $HYP_N_WARMUP \
     --runs $HYP_N_RUNS \
     --export-csv $RES_DIR/"my_cro_cpu.csv" \
-    --parameter-list n_population $N_SMALL
+    --parameter-list n_population $N_LARGE
 
 hyperfine "python $ALG_DIR/my_abc.py cuda 500 {n_population}" \
     --warmup $HYP_N_WARMUP \
@@ -35,7 +35,7 @@ hyperfine "python $ALG_DIR/my_abc.py cpu 500 {n_population}" \
     --warmup $HYP_N_WARMUP \
     --runs $HYP_N_RUNS \
     --export-csv $RES_DIR/"my_abc_cpu.csv" \
-    --parameter-list n_population $N_SMALL
+    --parameter-list n_population $N_LARGE
 
 hyperfine "python $ALG_DIR/my_sa.py cuda {n_dim}" \
     --warmup $HYP_N_WARMUP \
@@ -47,7 +47,7 @@ hyperfine "python $ALG_DIR/my_sa.py cpu {n_dim}" \
     --warmup $HYP_N_WARMUP \
     --runs $HYP_N_RUNS \
     --export-csv $RES_DIR/"my_sa_cpu.csv" \
-    --parameter-list n_dim "10,20,50,100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800,102400,204800,409600,819200,1638400,3276800,6553600"
+    --parameter-list n_dim "10,20,50,100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800,51200,102400,204800,409600,819200,1638400,3276800,6553600,13107200,26214400,52428800,104857600"
 
 ###############################################################################
 # theirs                                                                      #
